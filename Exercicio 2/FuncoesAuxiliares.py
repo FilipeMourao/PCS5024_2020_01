859ce0bfe0fcb384ab94506c1a7c4f1f8f01f7f9
Filipe Assis Mourão <ext.filipe.mourao@nexaresources.com>
2020-04-15 17:23:44 -0300

    exercicio 2 finalizado

--- Exercicio 2/FuncoesAuxiliares.py
+++ Exercicio 2/FuncoesAuxiliares.py
@@ -3,9 +3,6 @@ import pandas as pd
 import sklearn 
 import functools
 import xlrd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
@@ -32,28 +29,14 @@ def saveDescriptionOfEachColumn(datasetPath,pathToSaveTable,tableName):
     datasetColumns = datasetDataFrame.columns
     describedDataFrame = pd.DataFrame(columns = None)
     for column in datasetColumns:
-        describedDataFrame[column] = datasetDataFrame[column].describe()
+        describedDataFrame[column] = datasetDataFrame[column].describe(include='all')
     describedDataFrame.to_excel(pathToSaveTable+tableName)
-
-def getOccurencesOfMissingValuesByColumns(datasetDataFrame):
-    dicOfMissingValues = {}
-    sizeOfData = len(datasetDataFrame.index)
-    for column in datasetDataFrame.columns:
-        dicOfMissingValues[column] = list(datasetDataFrame[column]).count("?")*100/sizeOfData
-    return(dicOfMissingValues)
 
 def removeColumnsWithMissingValues(datasetDataFrame):
     dicOfMissingValues = getOccurencesOfMissingValuesByColumns(datasetDataFrame)
     columnsToRemove = [key for key,val  in dicOfMissingValues.items() if val > 0 ]
     datasetDataFrameWithoutColumnsWithMissingValues = datasetDataFrame.drop(columns = columnsToRemove)
     return datasetDataFrameWithoutColumnsWithMissingValues
-
-def printOccurencesAndPercentagesOfMissingValuesByColumns(datasetPath):    
-    datasetDataFrame = getDataframeInSpecificFormat(datasetPath)
-    dicOfMissingValues = getOccurencesOfMissingValuesByColumns(datasetDataFrame)
-    print("Porcentagem de '?' em cada coluna ")
-    for key,val in  dicOfMissingValues.items(): 
-        print('{key} - {val:.2f}'.format(key = key, val = val))
 
 def removeLinesWithMissingValues(datasetDataFrame):
     linesToBeRemoved = []
@@ -75,18 +58,6 @@ def getCorrelatedDataFrame(datasetDataFrame,outputName,numberOfRelevantVariables
     dummiesDataFrame = pd.get_dummies(datasetDataFrame)
     return dummiesDataFrame.drop(columns = columnsNames)
 
-def saveCorrelationImage(datasetDataFrame,folderPath,imageName,outputName,numberOfRelevantVariables):
-    dummiesDataFrame = pd.get_dummies(datasetDataFrame)
-    cor_target =  getCorrelationInDataframe(datasetDataFrame,outputName)
-    columnsNamesToBeRemoved = list(cor_target.keys()[numberOfRelevantVariables:])
-    correlatedDataFrame = dummiesDataFrame.drop(columns = columnsNamesToBeRemoved)
-    plt.figure(figsize=(50,40))
-    cor = correlatedDataFrame.corr()
-    sns.set(font_scale=4)
-    sns.heatmap(cor, annot=True, cmap=plt.cm.Reds, annot_kws={"size": 50})
-    # sns.heatmap(cor, annot=True, cmap=plt.cm.Reds,xticklabels=columns, yticklabels=columns)
-    plt.savefig(folderPath+imageName)
-
 def prepareDatasetforTraining(datasetDataFrame,outputName,numberOfRelevantVariables):
     dataFrame  = getCorrelatedDataFrame(datasetDataFrame,outputName,numberOfRelevantVariables)
     columns = list(dataFrame.columns)
@@ -97,5 +68,19 @@ def prepareDatasetforTraining(datasetDataFrame,outputName,numberOfRelevantVariab
     dataScaled = preprocessing.MinMaxScaler().fit_transform(data)
     df = pd.DataFrame(dataScaled)
     df.columns = columns
-    X_train, X_test, y_train, y_test = train_test_split(df.drop(columns = outputName).to_numpy() ,df[outputName].to_numpy() , test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
+    X_intermediary, X_test, y_intermediary, y_test = train_test_split(df.drop(columns = outputName).to_numpy() ,df[outputName].to_numpy() , test_size=0.2, random_state=42)
+    X_train, X_crossVal, y_train, y_crossVal = train_test_split(X_intermediary ,y_intermediary , test_size=0.25, random_state=42)
+    return X_train,X_crossVal, X_test,y_train,y_crossVal, y_test
+def prepareDatasetforTrainingComparationWithKaggle(datasetDataFrame,outputName,numberOfRelevantVariables):
+    #necessario para comparar com o resultado de 88% obtido por um competidor do kaggle
+    dataFrame  = getCorrelatedDataFrame(datasetDataFrame,outputName,numberOfRelevantVariables)
+    columns = list(dataFrame.columns)
+    columns.remove(outputName)
+    columns.append(outputName)
+    dataFrame = dataFrame[columns]
+    data = dataFrame.to_numpy()  
+    dataScaled = preprocessing.MinMaxScaler().fit_transform(data)
+    df = pd.DataFrame(dataScaled)
+    df.columns = columns
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(columns = outputName).to_numpy() ,df[outputName].to_numpy() , test_size=0.2, random_state=100)
+    return X_train, X_test,y_train, y_test
--- Exercicio 2/FuncoesDeMachineLearning.py
+++ Exercicio 2/FuncoesDeMachineLearning.py
@@ -4,38 +4,99 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
-def applyKNNWithDifferentHyperparameters(dataFrame, outputPath,fileName,outputName,numberOfRelevantVariables):
+from sklearn.naive_bayes import GaussianNB,MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+def applyKNNWithDifferentHyperparameters(X_train, X_test, y_train, y_test,numberOfRelevantVariables):
     listOfDicts = []
-    numberOfNeighboorsList = [10,20,30,50]
-    for numberOfVariables in numberOfRelevantVariables:
-        X_train, X_test, y_train, y_test = fa.prepareDatasetforTraining(dataFrame,outputName,numberOfVariables)
-        for numberOfNeighboorks in numberOfNeighboorsList:
-            peformance_dict = {}
-            peformance_dict['number of relevant variables'] = numberOfVariables
-            peformance_dict['number of neighbors'] = numberOfNeighboorks
-            neigh = KNeighborsClassifier(n_neighbors=numberOfNeighboorks)
-            y_predict = neigh.fit(X_train, y_train).predict(X_test)
-            metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
-            peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
-            peformance_dict['precision'] = metrics[0]
-            peformance_dict['recall'] = metrics[1]
-            peformance_dict['f1-score'] = metrics[2]
-            listOfDicts.append(peformance_dict)
-    peformanceDataFrame = pd.DataFrame(listOfDicts)
-    peformanceDataFrame.to_excel(outputPath+fileName,index = False)
-def applyNaiveBayesWithDifferentHyperparameters(dataFrame, outputPath,fileName,outputName,numberOfRelevantVariables):
-    listOfDicts = []
-    for numberOfVariables in numberOfRelevantVariables:
-        X_train, X_test, y_train, y_test = fa.prepareDatasetforTraining(dataFrame,outputName,numberOfVariables)
+    numberOfNeighboorsList = [10,20,30,50,60,70,80,90,100]
+    for numberOfNeighboorks in numberOfNeighboorsList:
         peformance_dict = {}
-        peformance_dict['number of relevant variables'] = numberOfVariables
-        y_predict = GaussianNB().fit(X_train, y_train).predict(X_test)
+        peformance_dict['number of neighbors'] = numberOfNeighboorks
+        neigh = KNeighborsClassifier(n_neighbors=numberOfNeighboorks)
+        y_predict = neigh.fit(X_train, y_train).predict(X_test)
         metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
+        peformance_dict['number of relevant variables'] = numberOfRelevantVariables
+        peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
+        peformance_dict['precision'] = metrics[0]
+        peformance_dict['recall'] = metrics[1]
+        peformance_dict['f1-score'] = metrics[2]
+        listOfDicts.append(peformance_dict)
+    peformanceDataFrame = pd.DataFrame(listOfDicts) 
+    return peformanceDataFrame
+def applyNaiveBayesWithDifferentHyperparameters(X_train, X_test, y_train, y_test,numberOfRelevantVariables):
+    naiveBayesOptimization = ['gaussian','multinominal','bernouli']
+    listOfDicts = []
+    for naiveBayesOptimizationParameter in naiveBayesOptimization: 
+        peformance_dict = {}
+        peformance_dict['method optimization'] = naiveBayesOptimizationParameter
+        if naiveBayesOptimizationParameter == 'gaussian':
+            nbo = GaussianNB()
+        elif naiveBayesOptimizationParameter == 'multinominal' : 
+            nbo = MultinomialNB()
+        else:
+            nbo = BernoulliNB()
+        y_predict = nbo.fit(X_train, y_train).predict(X_test)
+        metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
+        peformance_dict['number of relevant variables'] = numberOfRelevantVariables
         peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
         peformance_dict['precision'] = metrics[0]
         peformance_dict['recall'] = metrics[1]
         peformance_dict['f1-score'] = metrics[2]
         listOfDicts.append(peformance_dict)
     peformanceDataFrame = pd.DataFrame(listOfDicts)
-    peformanceDataFrame.to_excel(outputPath+fileName,index = False)
+    return peformanceDataFrame
+def applyLogisticRegressionDifferentHyperparameters(X_train, X_test, y_train, y_test,numberOfRelevantVariables):
+    listOfDicts = []
+    Cs = [2**i for i in range (-5,5)]
+    for C in Cs:
+        peformance_dict = {}
+        peformance_dict['C'] = C
+        y_predict = LogisticRegression(C=C,random_state=0).fit(X_train, y_train).predict(X_test)
+        metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
+        peformance_dict['number of relevant variables'] = numberOfRelevantVariables
+        peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
+        peformance_dict['precision'] = metrics[0]
+        peformance_dict['recall'] = metrics[1]
+        peformance_dict['f1-score'] = metrics[2]
+        listOfDicts.append(peformance_dict)
+    peformanceDataFrame = pd.DataFrame(listOfDicts) 
+    return peformanceDataFrame
+def applyDecisionTreeDifferentHyperparameters(X_train, X_test, y_train, y_test,numberOfRelevantVariables):
+    listOfDicts = []
+    max_depths = [int(x) for x in np.linspace(10, 110, num = 11)]
+    for max_depth in max_depths:
+        peformance_dict = {}
+        peformance_dict['max_depths'] = max_depth
+        dTC = tree.DecisionTreeClassifier()
+        y_predict = dTC.fit(X_train, y_train).predict(X_test)
+        metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
+        peformance_dict['number of relevant variables'] = numberOfRelevantVariables
+        peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
+        peformance_dict['precision'] = metrics[0]
+        peformance_dict['recall'] = metrics[1]
+        peformance_dict['f1-score'] = metrics[2]
+        listOfDicts.append(peformance_dict)
+    peformanceDataFrame = pd.DataFrame(listOfDicts) 
+    return peformanceDataFrame
+def applyRandomForestDifferentHyperparameters(X_train, X_test, y_train, y_test,numberOfRelevantVariables):
+    listOfDicts = []
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 5)]
+    max_depths = [int(x) for x in np.linspace(10, 110, num = 5)]
+    for n_estimator in n_estimators:
+        for max_depth in max_depths:
+            peformance_dict = {}
+            peformance_dict['max_depth'] = max_depth
+            peformance_dict['n_estimator'] = n_estimator
+            rFC = RandomForestClassifier(n_estimators=n_estimator,max_depth=max_depth, random_state=0)
+            y_predict = rFC.fit(X_train, y_train).predict(X_test)
+            metrics = precision_recall_fscore_support(np.array(y_test), y_predict,average = 'binary')
+            peformance_dict['number of relevant variables'] = numberOfRelevantVariables
+            peformance_dict['accuracy'] = accuracy_score(np.array(y_test), y_predict)
+            peformance_dict['precision'] = metrics[0]
+            peformance_dict['recall'] = metrics[1]
+            peformance_dict['f1-score'] = metrics[2]
+            listOfDicts.append(peformance_dict)
+    peformanceDataFrame = pd.DataFrame(listOfDicts) 
+    return peformanceDataFrame
